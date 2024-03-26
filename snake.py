e259d3f3b86dd18ab89279b1bb7f1f1b7ded63df
Faeze <faezeahmadi7173@gmail.com>
2024-03-26 16:05:06 +0330

    checking_re_moudule

--- snake.py
+++ snake.py
@@ -1,5 +1,14 @@
-inputs = input("Check the movement of the snake: ")
+import re
 
-if len(inputs) == 7:
-    lst = list(inputs)
-    print(lst)
+input_str = input("Check the movement of the snake: ")
+
+if len(input_str) == 7:
+    pattern = re.compile(r'ff|ll')
+    matches = pattern.findall(input_str)
+
+    if matches:
+        print("Pattern found in the input string.")
+        for match in matches:
+            print(match)
+    else:
+        print("Pattern not found in the input string.")
