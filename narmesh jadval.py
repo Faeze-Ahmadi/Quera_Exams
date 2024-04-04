c8f73f783bb091e2b6276d1b80ef50eb37f18428
Faeze Ahmadi <139007806+Faeze-Ahmadi@users.noreply.github.com>
2024-04-04 16:27:33 +0330

    Add files via upload

---
+++ narmesh jadval.py
@@ -0,0 +1,14 @@
+
+n = int(input())
+
+summation = 0
+summation2 = 0
+for i in range(n):
+    temp = [j for j in input()]
+q = int(input())
+s = input()
+
+for j in range(n):
+    for h in range(n):
+        print('w', end='')
+    print()
