64d105fd75c3ae09c52955ff4cac819834fce26c
Faeze Ahmadi <139007806+Faeze-Ahmadi@users.noreply.github.com>
2024-04-04 16:26:34 +0330

    Add files via upload

---
+++ 2 shoghle.py
@@ -0,0 +1,9 @@
+
+day_name = input()
+
+if day_name == 'shanbe' or day_name == 'doshanbe' or day_name == 'chaharshanbe':
+    print('perspolis')
+elif day_name == 'yekshanbe' or day_name == 'seshanbe' or day_name == 'panjshanbe':
+    print('bahman')
+else:
+    print('tatil')
